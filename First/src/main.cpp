32ad70fefa16c9273aadf74d35d7a98dee614e6d
thrt520 <610162816@qq.com>
2020-01-05 15:12:31 +0800

    Lessong2:triangle1

--- First/src/main.cpp
+++ First/src/main.cpp
@@ -8,11 +8,11 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green   = TGAColor(0, 255,   0,   255);
 Model *model = NULL;
-const int width = 800;
-const int height = 800;
-
-void line(int x0, int y0, int x1, int y1, TGAImage &img, TGAColor color) { 
+const int width = 200;
+const int height = 200;
+void line2(int x0, int y0, int x1, int y1, TGAImage &img, TGAColor color){
     bool steep = false; 
     if (std::abs(x0-x1)<std::abs(y0-y1)) { 
         std::swap(x0, y0); 
@@ -47,37 +47,76 @@ void line(int x0, int y0, int x1, int y1, TGAImage &img, TGAColor color) {
             }
         }
     }
+}
+void line(Vec2i t0, Vec2i t1, TGAImage &img, TGAColor color) { 
+	int x0, y0, x1, y1;
+	x0 = t0.x;
+	y0 = t0.y;
+	x1 = t1.x;
+	y1 = t1.y;
+    line2(t0.x, t0.y, t1.x, t1.y, img, color);
 } 
 
+
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color){
+	if(t0.y>t1.y) std::swap(t0, t1);
+	if(t0.y>t2.y) std::swap(t0, t2);
+	if(t1.y>t2.y) std::swap(t1, t2);
+	line(t0, t1, image, green);
+	line(t1, t2, image, green);
+	line(t2, t0, image, red);
+    int total_height = t2.y - t0.y;
+    float invslope1 = (t1.x - t0.x) / (float)(t1.y - t0.y);
+    float invslope2 = (t2.x - t0.x) / (float)(t2.y - t0.y);
+    int len = t1.y - t0.y;
+    for (int i = 0; i <= len; i++)
+    {
+        line2(t0.x + (int)(i * invslope1), t0.y + i, t0.x +(int)( i * invslope2 ), t0.y + i, image, color);
+    }
+    float invslope3 = (t0.x - t2.x)/(float)(t0.y - t2.y);
+    float invslope4 = (t1.x - t2.x)/(float)(t1.y - t2.y);
+    len = t2.y - t1.y;
+    for (int i = 0; i < len; i++)
+    {
+        line2(t2.x - (int)(i*invslope3), t2.y-i, t2.x-(int)(i*invslope4), t2.y-i, image, color);
+    }
+    
+}
+
+Vec3f barycentric(Vec2i *pts, Vec2i p){
+    // Vec3f u = 
+}
+
+void triangle2(Vec2i *pts, TGAImage &image, TGAColor color){
+    Vec2i bboxmin(image.get_width()-1, image.get_height()-1);
+    Vec2i bboxmax(0,0);
+    Vec2i clamp(image.get_width()-1, image.get_height()-1);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            // bboxmax[j]=std::
+        }
+        
+    }
+    
+}
+
+
+
 int main(int argc, char** argv) {
-	if(2==argc){
-		model= new Model(argv[1]);
-	}else{
-		model = new Model("D:/project/TinyRenderer/First/src/obj/african_head.obj");
-	}
 	TGAImage image(width, height, TGAImage::RGB);
-	std::cerr<<"nfaces"<<model->nfaces();
-	for (int i = 0; i < model->nfaces(); i++)
-	{
-		std::vector<int> face = model->face(i);
-		for (int j = 0; j < 3; j++)
-		{
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j+1)%3]);
-			int x0 = (v0.x + 1.)*width/2;
-			int y0 = (v0.y + 1.)*height/2;
-			int x1 = (v1.x + 1.)*width/2;
-			int y1 = (v1.y + 1.)*height/2;
-			line(x0,y0,x1,y1,image,white);
-		}
-		
-	}
-	
-	// line(0,0,100,100,image,white);
-	// line(100,100,0,0,image,red);
+	Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)}; 
+	Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)}; 
+	Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)}; 
+	triangle(t0[0], t0[1], t0[2],image , red);
+	triangle(t1[0], t1[1], t1[2],image , red);
+	triangle(t2[0], t2[1], t2[2],image , red);
 	image.flip_vertically();
-	image.write_tga_file("output_face.tga" , false);
-	system("Pause");
+	image.write_tga_file("output_triangle.tga" , false);
+	// system("Pause");
+
 	return 0;
 }
 
